da9574f40b5b8c90008124d021ed6ec1dc7e40a9
cqh6666 <2018ch@m.scnu.edu.cn>
2020-10-26 23:50:30 +0800

    delete fit_sinx.ipynb,update fit_sinx.py

--- fit_sinx.py
+++ fit_sinx.py
@@ -1,22 +1,26 @@
-
-
-from torch.utils.data import DataLoader,TensorDataset
-import torch.nn as nn
-import numpy as np
 import torch
+from torch.utils.data import DataLoader, TensorDataset
+import torch.nn as nn
+import matplotlib.pyplot as plt
+import numpy as np
+
+data_x = np.linspace(-2.0 * np.pi, 2.0 * np.pi, 500)
+data_y = np.sin(data_x)
 
 
-x = np.linspace(-2.0 * np.pi, 2.0 * np.pi, 500)
-np.random.shuffle(x)
-y = np.sin(x)
+def normalize_data(X, Y):
+    """变换为 [n,1] 格式，符合torch格式"""
+    X = np.expand_dims(X, axis=1)
+    Y = Y.reshape(500, -1)
+    return X, Y
 
-x = np.expand_dims(x,axis=1)
 
-
-y = y.reshape(500,-1)
-
-dataset = TensorDataset(torch.tensor(x,dtype=torch.float),torch.tensor(y,dtype=torch.float))
-dataloader = DataLoader(dataset,batch_size=100,shuffle=True)
+def transform_dataloader(data_X, data_Y):
+    """转化为pytorch的dataloader格式"""
+    X, Y = normalize_data(data_X, data_Y)
+    dataset = TensorDataset(torch.tensor(X, dtype=torch.float), torch.tensor(Y, dtype=torch.float))
+    dataloader = DataLoader(dataset, batch_size=100, shuffle=True)
+    return dataloader
 
 
 class Net(nn.Module):
@@ -32,3 +36,47 @@ class Net(nn.Module):
     def forward(self, input: torch.FloatTensor):
         return self.net(input)
 
+
+net = Net()
+
+
+def train(dataloader):
+    """训练"""
+    data_X, data_Y = normalize_data(data_x, data_y)
+    # 优化器 和 损失函数
+    optim = torch.optim.Adam(net.parameters(net), lr=0.001)
+    Loss = nn.MSELoss()
+
+    for epoch in range(1000):
+        loss = None
+        for batch_x, batch_y in dataloader:
+            y_predict = net(batch_x)
+            loss = Loss(y_predict, batch_y)
+            optim.zero_grad()
+            loss.backward()
+            optim.step()
+
+        if (epoch + 1) % 100 == 0:
+            print("step: {0} , loss: {1}".format(epoch + 1, loss.item()))
+
+    print("训练完成......")
+
+
+def plot_sin():
+    test_x = np.linspace(-2.0 * np.pi, 2.0 * np.pi, 100)
+    test_X = np.expand_dims(test_x, axis=1)
+
+    plt.plot(data_x, data_y, label="fact")
+    predict = net(torch.tensor(test_X, dtype=torch.float))
+    plt.plot(test_X, predict.detach().numpy(), marker='x', label="predict")
+    plt.title("fit_sin")
+    plt.xlabel("x")
+    plt.ylabel("sin(x)")
+    plt.legend()
+    plt.show()
+
+
+if __name__ == '__main__':
+    dataloader = transform_dataloader(data_x, data_y)
+    train(dataloader)
+    plot_sin()
